e266487a43c9eddd99acc01009620e16fe891a22
AG-444 <aditya444garg@gmail.com>
2025-07-08 22:52:13 +0530

    V105 Add validation for user and place routes; update controllers and package dependencies

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+const {validationResult} = require('express-validator');
 
 
 const HttpError = require('../models/http-error');
@@ -51,6 +52,13 @@ const getPlacesByUserId = (req, res, next) => {
 };
 
 const createPlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors);
+    res.status(422);
+    throw new HttpError('Invalid inputs passed',422);
+  }
+
   const { title, description, coordinates, address, creator } = req.body;
   // const title = req.body.title;
   const createdPlace = {
@@ -68,6 +76,13 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    console.log(errors);
+    res.status(422);
+    throw new HttpError('Invalid inputs passed',422);
+  }
+
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -83,6 +98,11 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+
+  if(!DUMMY_PLACES.find(p => p.id ===placeId)){
+    throw new HttpError('Couldnt find place for that id',404);
+  }
+
   DUMMY_PLACES = DUMMY_PLACES.filter(p => p.id !== placeId);
   res.status(200).json({ message: 'Deleted place.' });
 };
