10e4a459449826bdc3c3fffca91900e5876d11ba
AG-444 <aditya444garg@gmail.com>
2025-07-08 21:32:53 +0530

    V100  Implement create, update, and delete functions in places controller; update routes accordingly

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -1,4 +1,5 @@
 const { v4: uuidv4 } = require('uuid');
+
 const HttpError = require('../models/http-error');
 
 const DUMMY_PLACES = [
@@ -48,11 +49,11 @@ const getPlaceByUserId = (req, res, next) => {
   res.json({ place });
 };
 
-
-const createPlace = (req,res,next) =>{
-  const { title, description, coordinates, address, creator} = req.body;
+const createPlace = (req, res, next) => {
+  const { title, description, coordinates, address, creator } = req.body;
+  // const title = req.body.title;
   const createdPlace = {
-    id:uuidv4(),
+    id: uuid(),
     title,
     description,
     location: coordinates,
@@ -60,11 +61,29 @@ const createPlace = (req,res,next) =>{
     creator
   };
 
-  DUMMY_PLACES.push(createdPlace);
+  DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
 
-  res.status(201).json({place:createdPlace});
+  res.status(201).json({ place: createdPlace });
 };
+
+const updatePlace = (req, res, next) => {
+  const { title, description } = req.body;
+  const placeId = req.params.pid;
+
+  const updatedPlace = { ...DUMMY_PLACES.find(p => p.id === placeId) };
+  const placeIndex = DUMMY_PLACES.findIndex(p => p.id === placeId);
+  updatedPlace.title = title;
+  updatedPlace.description = description;
+
+  DUMMY_PLACES[placeIndex] = updatedPlace;
+
+  res.status(200).json({place: updatedPlace});
+};
+
+const deletePlace = (req, res, next) => {};
 
 exports.getPlaceById = getPlaceById;
 exports.getPlaceByUserId = getPlaceByUserId;
 exports.createPlace = createPlace;
+exports.updatePlace = updatePlace;
+exports.deletePlace = deletePlace;
