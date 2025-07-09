c13a0325444452d9b84aeac815550e6ca2e01540
AG-444 <aditya444garg@gmail.com>
2025-07-09 23:07:45 +0530

    v126 Add mongoose connection to app and update package dependencies

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const placesRoutes = require('./routes/places-routes');
 const usersRoutes = require('./routes/users-routes');
@@ -25,4 +26,11 @@ app.use((error, req, res, next) => {
   res.json({message: error.message || 'An unknown error occurred!'});
 });
 
-app.listen(5000);
+mongoose
+  .connect('mongodb://Adi4:LAMPER1234@ac-qvstyix-shard-00-00.7jhaea5.mongodb.net:27017,ac-qvstyix-shard-00-01.7jhaea5.mongodb.net:27017,ac-qvstyix-shard-00-02.7jhaea5.mongodb.net:27017/places?ssl=true&replicaSet=atlas-tfk6bm-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Lenscape-users')
+  .then(()=>{
+    app.listen(5000);
+  })
+  .catch(err=>{
+    console.log(err);
+  });
