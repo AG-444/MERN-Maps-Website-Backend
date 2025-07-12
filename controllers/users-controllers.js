024f01bb5558af06292f9ad219e4d14531ed0f83
AG-444 <aditya444garg@gmail.com>
2025-07-12 19:38:35 +0530

    V179 Hashing password using bcrypt before storing on Database

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -1,4 +1,5 @@
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const HttpError = require('../models/http-error');
 const User = require('../models/user');
@@ -46,11 +47,20 @@ const signup = async (req, res, next) => {
     return next(error);
   }
 
+  let hashedPassword;
+  try{  
+    hashedPassword = await bcrypt.hash(password, 12);
+  } catch(err){
+    const error = new HttpError('Could not create user, please try again',500);
+    return next(error);
+  }
+
+  
   const createdUser = new User({
     name,
     email,
     image: req.file.path,
-    password,
+    password: hashedPassword,
     places: []
   });
 
@@ -81,8 +91,9 @@ const login = async (req, res, next) => {
     );
     return next(error);
   }
+  
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
       'Invalid credentials, could not log you in.',
       401
@@ -90,6 +101,18 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
+  let isValidPassword = false;
+  try {  
+    isValidPassword = await bcrypt.compare(password,existingUser.password);
+  } catch (err){
+    const error = new HttpError('Could not log you in, please check your credentials and try again',500);
+    return next(error);
+  }
+
+  if(!isValidPassword){
+    const error = new HttpError('Passwords dont match, please check your credentials and try again',500);
+    return next(error);
+  }
   res.json({
     message: 'Logged in!',
     user: existingUser.toObject({ getters: true })
